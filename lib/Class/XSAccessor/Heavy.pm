fe71cd61abb23627b4e05435e4c581d6c8ae7538
smueller@cpan.org <smueller@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-09-06 10:37:50 +0000

    add empty Heavy.pm for the common guts
    git-svn-id: http://svn.ali.as/cpan/trunk/Class-XSAccessor@8882 88f4d9cd-8a04-0410-9d60-8f63309c3137

---
+++ lib/Class/XSAccessor/Heavy.pm
@@ -0,0 +1,47 @@
+package Class::XSAccessor::Heavy;
+
+use 5.006;
+use strict;
+use warnings;
+use Carp qw/croak/;
+
+our $VERSION = '1.04';
+
+
+1;
+__END__
+
+=head1 NAME
+
+Class::XSAccessor::Heavy - Guts you don't care about
+
+=head1 SYNOPSIS
+  
+  use Class::XSAccessor!
+
+=head1 DESCRIPTION
+
+Common guts for Class::XSAccessor and Class::XSAccessor::Array.
+No user-serviceable parts inside!
+
+=head1 SEE ALSO
+
+L<Class::XSAccessor>
+L<Class::XSAccessor::Array>
+
+=head1 AUTHOR
+
+Steffen Mueller, E<lt>smueller@cpan.orgE<gt>
+
+Chocolateboy, E<lt>chocolate@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2008-2009 by Steffen Mueller
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
+
