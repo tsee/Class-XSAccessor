8ed20801520c289ee637d68eb7713f75d56cdb79
chocolateboy <chocolate@cpan.org>
2020-10-27 17:52:35 +0000

    fix compiler warnings

--- MurmurHashNeutral2.h
+++ MurmurHashNeutral2.h
@@ -13,44 +13,49 @@
 #define _MurmurHashNeutral2_h_
 
 U32 CXSA_MurmurHashNeutral2(const void* key, STRLEN len, U32 _seed) {
-  const unsigned int m = 0x5bd1e995;
-  const int r = 24;
+    const unsigned int m = 0x5bd1e995;
+    const int r = 24;
 
-  unsigned int h = _seed ^ len;
+    unsigned int h = _seed ^ len;
 
-  const unsigned char* data = (const unsigned char*)key;
+    const unsigned char* data = (const unsigned char*)key;
 
-  while(len >= 4) {
-    unsigned int k;
+    while (len >= 4) {
+        unsigned int k;
 
-    k  = data[0];
-    k |= data[1] << 8;
-    k |= data[2] << 16;
-    k |= data[3] << 24;
+        k  = data[0];
+        k |= data[1] << 8;
+        k |= data[2] << 16;
+        k |= data[3] << 24;
 
-    k *= m; 
-    k ^= k >> r; 
-    k *= m;
+        k *= m;
+        k ^= k >> r;
+        k *= m;
 
+        h *= m;
+        h ^= k;
+
+        data += 4;
+        len -= 4;
+    }
+
+    switch(len)  {
+        case 3:
+            h ^= data[2] << 16;
+            /* fall through */
+        case 2:
+            h ^= data[1] << 8;
+            /* fall through */
+        case 1:
+            h ^= data[0];
+            h *= m;
+    };
+
+    h ^= h >> 13;
     h *= m;
-    h ^= k;
+    h ^= h >> 15;
 
-    data += 4;
-    len -= 4;
-  }
-  
-  switch(len)  {
-  case 3: h ^= data[2] << 16;
-  case 2: h ^= data[1] << 8;
-  case 1: h ^= data[0];
-          h *= m;
-  };
-
-  h ^= h >> 13;
-  h *= m;
-  h ^= h >> 15;
-
-  return (U32)h;
-} 
+    return (U32)h;
+}
 
 #endif
